819391f002386736dae395962482405c5548e00b
Ben DeZutti <benjamindezutti@gmail.com>
2017-06-14 22:36:53 -0400

    commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: Any) {
        tapCount = tapCount+1
         print(tapCount)
-        if tapCount > 10 {
+        if tapCount > 20 {
             thescoreboard.text = "You win!"
             
             if tapCount > 30 {
