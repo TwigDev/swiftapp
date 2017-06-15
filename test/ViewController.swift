c03fc0a4c5ef55e22393bf5d6fc596c9bccfae36
Ben DeZutti <benjamindezutti@gmail.com>
2017-06-14 22:33:44 -0400

    First commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thescoreboard: UILabel!
+   
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+       tapCount = tapCount+1
+        print(tapCount)
+        if tapCount > 10 {
+            thescoreboard.text = "You win!"
+            
+            if tapCount > 30 {
+                thescoreboard.text = "Scoreboard"
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
